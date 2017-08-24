9e45d5c009e1a8d84e312189a007c755e3e621bf
marcio-costa <marcioacosta@gmail.com>
2017-08-23 23:21:14 -0300

    terminei...

--- exec03.rb
+++ exec03.rb
@@ -2,23 +2,20 @@ require 'logger'
 
 $log = Logger.new('exec03.log')
 
-ip_disponivel = File.read("./ip_livres")
-ip_indisponiveis = File.open("./ip_indisponiveis", "w")
+ip_disponivel = File.readlines("./ip_livres")
+ip_indisponiveis = File.readlines("./ip_indisponiveis")
 
-puts ip_disponivel
-
-#for ip in ip_disponivel
-
+achou = 0
 ip_disponivel.each do |ip|
-    if ip_indisponiveis.include?(ip) == TRUE
-        ip_indisponiveis.gsub!(/(|\s){ip}(|\s)/, "").chomp!
-        ip_indisponiveis = ip_indisponiveis.gsub(/^$\n/, '')
-    elsif ip_indisponiveis.include?(ip) == FALSE
-        ip_indisponiveis << ip
+    if ip_indisponiveis.include?(ip) != TRUE
+        File.open("./ip_indisponiveis", "a+") { |f| f << "#{ip}" }
         puts "Nome do Solicitante?" 
         solicitante = gets.chomp
         $log.info("O Solicitante #{solicitante} recebeu o seguinte ip livre #{ip}")
-    else 
-        print "Não mais ip disponivel"
+        achou = 1
+        break
     end
+end
+if achou == 0 
+    print "Não mais ip disponivel \n"
 end
